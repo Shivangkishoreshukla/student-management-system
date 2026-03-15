59ed22fbbed393429e193f1fe9325003cc47889c
devang1 <devangshukla119@gmail.com>
2026-03-16 00:59:36 +0530

    added

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api'; // Update this with your backend URL
+const API_BASE_URL = 'https://student-management-system-v0uv.onrender.com'; 
 
 const api = axios.create({
   baseURL: API_BASE_URL,
