84bb8edf39de3b3c950d710aae80991f7521d1ba
devang1 <devangshukla119@gmail.com>
2026-04-20 23:02:35 +0530

    updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://student-management-system-2-m693.onrender.com/api'; 
+const API_BASE_URL = process.env.REACT_APP_API_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
